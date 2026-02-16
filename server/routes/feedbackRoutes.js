5a7d653324c0ea2fae509ffff5be2bc4b45b4a1f
yash <ch.yashwanth777@gmail.com>
2026-02-16 13:33:24 +0530

    sentiment

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -2,10 +2,30 @@ const express = require("express");
 const router = express.Router();
 const Feedback = require("../models/Feedback");
 
+const { analyzeSentiment } = require("../services/aiSuggestionsService");
+
 // CREATE feedback
 router.post("/", async (req, res) => {
-  const feedback = await Feedback.create(req.body);
-  res.json(feedback);
+  try {
+    const { comment } = req.body;
+
+    // Calculate sentiment score from comment using LLM
+    let sentimentScore = 0.5;
+    if (comment) {
+      sentimentScore = await analyzeSentiment(comment);
+    }
+
+    // Override/Set the sentiment score
+    const feedbackData = {
+      ...req.body,
+      sentimentScore
+    };
+
+    const feedback = await Feedback.create(feedbackData);
+    res.json(feedback);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // GET feedback by manager
